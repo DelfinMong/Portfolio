ecbb85adcd713bf0b6e15dff1ee7dfd4237384f9
delfin mong <delfdevs@gmail.com>
2019-06-06 22:58:34 -0400

    landing page

---
+++ port.js
@@ -0,0 +1,16 @@
+// elements
+var $page = $('.page');
+
+$('.menu_toggle').on('click', function(){
+  $page.toggleClass('shazam');
+});
+$('.content').on('click', function(){
+  $page.removeClass('shazam');
+});
+
+
+
+
+
+
+
